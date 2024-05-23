10786c912ae431e97027407597cf345a15eecce6
Martina Emerson <wangdongovo@gmail.com>
2024-05-23 15:45:35 +0800

    feat: 初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,9 +14,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
