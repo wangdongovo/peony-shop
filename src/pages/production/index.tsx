3cecf6bae6ce13ca234fa3501c85fb0fe042d76c
wd <wangdongovo@gmail.com>
2024-06-08 09:13:22 -0700

    fix: /

--- src/pages/production/index.tsx
+++ src/pages/production/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
 
+interface State {
+  createTime: string
+  goodsId: string
+  goodsName: string
+  goodsPrice: string
+}
 
-const Production = () => {
+class Production extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      createTime: '2021-01-01',
+      goodsId: '123456',
+      goodsName: '测试商品',
+      goodsPrice: '100'
+    }
+  }
+
+  render() {
+    const { createTime, goodsName } = this.state
     return (
-        <div>
-            <h1>Production</h1>
-        </div>
+      <div>
+        产品展示页面
+        <p>创建时间：{createTime}</p>
+        <p>商品名称：{goodsName}</p>
+      </div>
     )
+  }
 }
 
 export default Production
