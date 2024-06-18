15cf77cd0c1083cc74283c087a10c20492cdc7ae
Martina Emerson <wangdongovo@gmail.com>
2024-06-18 11:41:19 +0800

    fix: /

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import './index.less'
 const Login = () => {
-
-
-
-
-
+  const navigate = useNavigate()
 
   // 登录
   const handleLogin = () => {
     console.log('登录')
+    navigate('/')
   }
   return (
     <div className="login-wrapper">
@@ -23,7 +21,9 @@ const Login = () => {
             <input type="password" placeholder="请输入密码" />
           </div>
 
-          <div className="button-section" onClick={handleLogin}>登录</div>
+          <div className="button-section" onClick={handleLogin}>
+            登录
+          </div>
         </div>
       </div>
     </div>
--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,21 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.less'
 
 const Me = () => {
+  const [userInfo, setUserInfo] = useState({
+    name: '芍药居小卖部',
+    uid: '243244',
+    intro: '有趣的简洁吸引粉丝',
+    avatar: 'https://sns-avatar-qc.xhscdn.com/avatar/5e1df3895b495a0001a02ff4.jpg',
+    sex: '男',
+    birthday: '2024-06-17',
+    phone: '18513389799',
+    email: 'caixukun@gmail.com',
+    address: '中国  背景  朝阳'
+  })
+
+
+
+
+
+
+
   return (
     <div className="me-container">
       <div className="avatar-section">
-        <img
-          src="https://sns-avatar-qc.xhscdn.com/avatar/5e1df3895b495a0001a02ff4.jpg"
-          alt=""
-          
-        />
+        <img src={userInfo.avatar} alt="" />
       </div>
 
-
-      <div className='list-section'>
-
+      <div className="list-section">
+        <div className="list-item">
+          <div className="label">名字</div>
+          <div className="content">{userInfo.name}</div>
+        </div>
+        <div className="list-item">
+          <div className="label">uid</div>
+          <div className="content">{userInfo.uid}</div>
+        </div>
+        <div className="list-item">
+          <div className="label">简介</div>
+          <div className="content">{userInfo.intro}</div>
+        </div>
         
+        <div className="list-item">
+          <div className="label">性别</div>
+          <div className="content">{userInfo.sex}</div>
+        </div>
+        <div className="list-item">
+          <div className="label">生日</div>
+          <div className="content">{userInfo.birthday}</div>
+        </div>
+        <div className="list-item">
+          <div className="label">手机号</div>
+          <div className="content">{userInfo.phone}</div>
+        </div>
+        <div className="list-item">
+          <div className="label">邮箱</div>
+          <div className="content">{userInfo.email}</div>
+        </div>
+        <div className="list-item">
+          <div className="label">地址</div>
+          <div className="content">{userInfo.address}</div>
+        </div>
       </div>
     </div>
   )
