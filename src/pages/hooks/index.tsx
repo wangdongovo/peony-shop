2420b4a822b6a8a0c7ba047a7d383b7f708b2d88
Martina Emerson <wangdongovo@gmail.com>
2024-06-20 16:37:13 +0800

    feat: 添加useContext

--- src/pages/hooks/index.tsx
+++ src/pages/hooks/index.tsx
@@ -4,8 +4,8 @@ import EffectHooks from './components/effect-hooks'
 import MemoHooks from './components/memo-hooks'
 import CallbackHooks from './components/callback-hooks'
 import IdHooks from './components/id-hooks'
-
 import RefHooks from './components/ref-hooks'
+import ContextHooks from './components/context-hooks'
 
 const ReactHooks = () => {
   return (
@@ -16,7 +16,8 @@ const ReactHooks = () => {
       {/* <MemoHooks /> */}
       {/* <CallbackHooks/> */}
       {/* <IdHooks /> */}
-      <RefHooks />
+      {/* <RefHooks /> */}
+      <ContextHooks />
     </div>
   )
 }
