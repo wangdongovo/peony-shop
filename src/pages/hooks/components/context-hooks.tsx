776a0a94c30b3a0aa2d7e152a6d8674dedcf5f81
Martina Emerson <wangdongovo@gmail.com>
2024-06-20 17:47:06 +0800

    feat: 添加useContext

--- src/pages/hooks/components/context-hooks.tsx
+++ src/pages/hooks/components/context-hooks.tsx
@@ -1,42 +1,36 @@
-
 import React, { createContext, useContext, useState } from 'react'
 
+const ThemeContext = createContext(null)
 
+const ContextHooks = () => {
+  const [theme, setTheme] = useState<any>('light')
 
+  return (
+    <div>
+      <h1>Context Hooks</h1>
 
+      <ThemeContext.Provider value={theme}>
+        <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Toggle Theme</button>
 
-const ThemeContext = createContext(null);
-
-const ContextHooks = () => {  
-
-    const [theme, setTheme] = useState<any>('light');
-    
-    
-    return(
-        <div>
-            <h1>Context Hooks</h1>
-
-
-            <ThemeContext.Provider value={theme}>
-                <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Toggle Theme</button>
-                
-                <Panel />
-            </ThemeContext.Provider>
-        </div>
-    )
+        <Panel />
+      </ThemeContext.Provider>
+    </div>
+  )
 }
-
-
 
 const Panel = () => {
-    const theme = useContext(ThemeContext);
+  const theme = useContext(ThemeContext)
 
-    return (
-        <div style={{ backgroundColor: theme === 'light' ? 'white' : 'black', color: theme === 'light' ? 'black' : 'white' }}>
-            <h2>Panel</h2>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        backgroundColor: theme === 'light' ? 'white' : 'black',
+        color: theme === 'light' ? 'black' : 'white'
+      }}
+    >
+      <h2>Panel</h2>
+    </div>
+  )
 }
-
 
 export default ContextHooks
